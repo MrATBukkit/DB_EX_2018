0f531154030fcbeec387a413c993a119b3964a7a
MrATBukkit <hewalzl999@gmail.com>
2018-10-24 17:26:30 +0200

    Basic Version

---
+++ RecipeGUI/RecipeGui.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace RecipeGUI
+{
+    partial class RecipeGui
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RecipeList = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rtb_output = new System.Windows.Forms.RichTextBox();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RecipeList
+            // 
+            this.RecipeList.FormattingEnabled = true;
+            this.RecipeList.ItemHeight = 16;
+            this.RecipeList.Location = new System.Drawing.Point(13, 45);
+            this.RecipeList.Name = "RecipeList";
+            this.RecipeList.Size = new System.Drawing.Size(235, 388);
+            this.RecipeList.TabIndex = 0;
+            this.RecipeList.SelectedIndexChanged += new System.EventHandler(this.RecipeList_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(13, 440);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(235, 41);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Hinzufügen";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // rtb_output
+            // 
+            this.rtb_output.Location = new System.Drawing.Point(254, 13);
+            this.rtb_output.Name = "rtb_output";
+            this.rtb_output.Size = new System.Drawing.Size(555, 420);
+            this.rtb_output.TabIndex = 3;
+            this.rtb_output.Text = "";
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(255, 440);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(284, 41);
+            this.btn_edit.TabIndex = 4;
+            this.btn_edit.Text = "Bearbeiten";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(545, 440);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(264, 41);
+            this.btn_delete.TabIndex = 5;
+            this.btn_delete.Text = "Löschen";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // RecipeGui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 493);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.rtb_output);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.RecipeList);
+            this.Name = "RecipeGui";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox RecipeList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox rtb_output;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
+
---
+++ RecipeGUI/RecipeGui.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Recipe;
+using RecipeStorageCSV;
+using RecipeTestApp;
+using Recipe;
+
+namespace RecipeGUI
+{
+    public partial class RecipeGui : Form
+    {
+        List<Recipe.Recipe> allRecipes = new List<Recipe.Recipe>();
+
+        public RecipeGui()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private static readonly String[] TEST_ING_NAMES = {
+                "Pfeffer",
+                "Salz",
+                "Mehl",
+                "Zucker",
+                "Hefe",
+                "Wasser"
+        };
+
+        private void init()
+        {
+            RecipeDataProviderImpl rsStorage = new RecipeDataProviderImpl("test.csv");
+            if (rsStorage.Ingredients.Count == 0)
+            {
+                foreach (String ingName in TEST_ING_NAMES)
+                {
+                    Ingredient tmpIng = new Ingredient();
+                    tmpIng.Name = ingName;
+                    System.Console.WriteLine("Add Ingredient: " + tmpIng);
+                    rsStorage.AddIngredient(tmpIng);
+                }
+            }
+            if (rsStorage.Recipes.Count == 0)
+            {
+                Recipe.Recipe kuchenRcp = new Recipe.Recipe();
+                kuchenRcp.Name = "Kuchen";
+                kuchenRcp.Text = "Zubereitung";
+                RecipeItem mehlItem = new RecipeItem();
+                mehlItem.Count = 1;
+                mehlItem.Unit = "Tasse";
+                mehlItem.Ingredient = rsStorage.Ingredients[2];
+                RecipeItem backPulverItem = new RecipeItem();
+                backPulverItem.Count = 2;
+                backPulverItem.Unit = "Messerspitze";
+                backPulverItem.Ingredient = new Ingredient();
+                backPulverItem.Ingredient.Name = "Backpulver";
+                kuchenRcp.addIngredient(mehlItem);
+                kuchenRcp.addIngredient(backPulverItem);
+                rsStorage.AddRecipe(kuchenRcp);
+                System.Console.WriteLine("Add Recipe: " + kuchenRcp);
+            }
+            foreach (Recipe.Recipe rcp in rsStorage.Recipes)
+            {
+                RecipeList.Items.Add(rcp.Name);
+                allRecipes.Add(rcp);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RecipeList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (RecipeList.SelectedIndex < 0)
+            {
+                return;
+            }
+            Recipe.Recipe item = allRecipes[RecipeList.SelectedIndex];
+            rtb_output.Text = item.ToString();
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            int index = RecipeList.SelectedIndex;
+            allRecipes.RemoveAt(index);
+            RecipeList.Items.RemoveAt(index);
+            rtb_output.Text = "";
+        }
+    }
+}
